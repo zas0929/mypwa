b5bc6471f2506c3014bcadd54991a34cf33d84d4
zas0929 <rolisovskiy@mail.ru>
2018-12-11 22:49:18 +0200

    Updates

---
+++ precache-manifest.2df33728b3fa97800b4f9ec15703d9d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mypwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e6770d678b4bb2a9792",
+    "url": "/mypwa/static/js/runtime~main.3e6770d6.js"
+  },
+  {
+    "revision": "b416b7dab5584b6108bf",
+    "url": "/mypwa/static/js/main.b416b7da.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/mypwa/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "b416b7dab5584b6108bf",
+    "url": "/mypwa/static/css/main.772d8358.chunk.css"
+  },
+  {
+    "revision": "b0b3a4c4dc330511498cdcf43c129be7",
+    "url": "/mypwa/index.html"
+  }
+];
