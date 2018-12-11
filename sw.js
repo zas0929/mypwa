3a3c996a8ff53d2af4628b3ad6ba85a5491f1049
zas0929 <rolisovskiy@mail.ru>
2018-12-11 23:46:59 +0200

    Updates

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html", "static/css*"]);
+toolbox.precache(["index.html", "static/css/*"]);
 
-toolbox.router.get('/static/media*', toolbox.cacheFirst);
+toolbox.router.get('/static/media/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
